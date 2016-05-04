9818ff32a0bec2b8cce153a8ae42ae56e640c57b
Fabio Tudone <fabio@paralleluniverse.co>
2016-05-04 12:03:52 +0300

    Align to latest blog post updates

--- src/main/java/SkynetChannels.java
+++ src/main/java/SkynetChannels.java
@@ -1,6 +1,7 @@
 import co.paralleluniverse.fibers.*;
 import co.paralleluniverse.strands.channels.Channel;
 import static co.paralleluniverse.strands.channels.Channels.*;
+import java.time.*;
 
 public class SkynetChannels {
     static void skynet(Channel<Long> c, long num, int size, int div) throws SuspendExecution, InterruptedException {
@@ -22,19 +23,17 @@ public class SkynetChannels {
 
     public static void main(String[] args) throws Exception {
         for (int i = 0 ; i < RUNS ; i++) {
-            long start = System.nanoTime();
+            Instant start = Instant.now();
 
             Channel<Long> c = newChannel(BUFFER);
-            new Fiber(() -> skynet(c, 0, TOTAL, BRANCH)).start();
+            new Fiber(() -> skynet(c, 0, 1_000_000, 10)).start();
             long result = c.receive();
 
-            long elapsed = (System.nanoTime() - start) / 1_000_000;
-            System.out.println((i+1) + ": " + result + " (" + elapsed + " ms)");
+            Duration elapsed = Duration.between(start, Instant.now());
+            System.out.println((i + 1) + ": " + result + " (" + elapsed.toMillis() + " ms)");
         }
     }
 
     static final int RUNS = 4;
-    static final int BRANCH = 10;
-    static final int BUFFER = -1; // >= 0 (fully sync), <= BRANCH (fully async) ; < 0 means unlimited
-    static final int TOTAL = 1_000_000; // >= BRANCH
+    static final int BUFFER = 10; // = 0 unbufferd, > 0 buffered ; < 0 unlimited
 }
